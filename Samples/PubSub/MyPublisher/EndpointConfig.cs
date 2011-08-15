1e212be651a60792cba9e5e9ec2098e5dcf93b8e
JimmyP <github@thejimmyp.com>
2011-08-15 10:35:31 +1000

    understand how NAnt scripts provides NSB binary references to all of the projects, still have some problems with 3rd party binary references for the NServiceBus.Unicast.Subscriptions.Raven.sln

--- Samples/PubSub/MyPublisher/EndpointConfig.cs
+++ Samples/PubSub/MyPublisher/EndpointConfig.cs
@@ -6,5 +6,15 @@ using NServiceBus.Sagas.Impl;
 namespace MyPublisher
 {
     class EndpointConfig : IConfigureThisEndpoint, AsA_Publisher,
-    IAmResponsibleForMessages<IEvent>, IAmResponsibleForMessages<EventMessage>{}
+    IAmResponsibleForMessages<IEvent>, IAmResponsibleForMessages<EventMessage>{} IWantCustomInitialization
+{
+    public void Init()
+    {
+        NServiceBus.Configure.With()
+            .CastleWindsorBuilder() // just to show we can mix and match containers
+            .XmlSerializer()
+            .UnicastBus()
+            .DoNotAutoSubscribe(); //managed by the class Subscriber2Endpoint
+    }
+}
 }
