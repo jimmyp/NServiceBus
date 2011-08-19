5f9a9d76e6642d18ba3e210fe66ce543499e3dec
unknown <github@thejimmyp.com>
2011-08-20 04:58:32 +1000

    added raven subscription storage to build script

--- Samples/PubSub/MyPublisher/EndpointConfig.cs
+++ Samples/PubSub/MyPublisher/EndpointConfig.cs
@@ -2,19 +2,21 @@
 using NServiceBus;
 using NServiceBus.Grid.MessageHandlers;
 using NServiceBus.Sagas.Impl;
+using NServiceBus.Unicast.Subscriptions.Raven.Config;
 
 namespace MyPublisher
 {
-    class EndpointConfig : IConfigureThisEndpoint, AsA_Publisher,
-    IAmResponsibleForMessages<IEvent>, IAmResponsibleForMessages<EventMessage>{} IWantCustomInitialization
-{
-    public void Init()
-    {
-        NServiceBus.Configure.With()
-            .CastleWindsorBuilder() // just to show we can mix and match containers
-            .XmlSerializer()
-            .UnicastBus()
-            .DoNotAutoSubscribe(); //managed by the class Subscriber2Endpoint
+    class EndpointConfig : IConfigureThisEndpoint, AsA_Publisher, IWantCustomInitialization,
+    IAmResponsibleForMessages<IEvent>, IAmResponsibleForMessages<EventMessage>{
+
+
+        public void Init()
+        {
+            NServiceBus.Configure.With()
+                .EmbeddedRavenSubscriptionStorage()
+                .XmlSerializer()
+                .UnicastBus()
+                .DoNotAutoSubscribe(); //managed by the class Subscriber2Endpoint
+        }
     }
-}
 }
