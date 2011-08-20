1a3679120ddcba0b0f3485279fa656dc553f8e92
unknown <github@thejimmyp.com>
2011-08-20 15:22:45 +1000

    Got external raven server working for subscription storage, pub sub sample is using it

--- Samples/PubSub/MyPublisher/EndpointConfig.cs
+++ Samples/PubSub/MyPublisher/EndpointConfig.cs
@@ -6,14 +6,19 @@ using NServiceBus.Unicast.Subscriptions.Raven.Config;
 
 namespace MyPublisher
 {
-    class EndpointConfig : IConfigureThisEndpoint, AsA_Publisher, IWantCustomInitialization,
+    class EndpointConfig : IConfigureThisEndpoint, AsA_Publisher,
     IAmResponsibleForMessages<IEvent>, IAmResponsibleForMessages<EventMessage>{
 
 
+        
+    }
+
+    public class Busconfig: IWantCustomInitialization
+    {
         public void Init()
         {
             NServiceBus.Configure.With()
-                .EmbeddedRavenSubscriptionStorage()
+                .RavenSubscriptionStorage("Server")
                 .XmlSerializer()
                 .UnicastBus()
                 .DoNotAutoSubscribe(); //managed by the class Subscriber2Endpoint
--- src/impl/unicast/NServiceBus.Unicast.Subscriptions.Raven/Subscription.cs
+++ src/impl/unicast/NServiceBus.Unicast.Subscriptions.Raven/Subscription.cs
@@ -1,4 +1,7 @@
-﻿namespace NServiceBus.Unicast.Subscriptions.Raven
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace NServiceBus.Unicast.Subscriptions.Raven
 {
     public class Subscription
     {
@@ -10,7 +13,30 @@
 
         public static string FormatId(string endpoint, string messageType, Address client)
         {
-            return string.Format("Subscriptions/{0}/{1}/{2}", endpoint, messageType, client);
+            var hunanReadableId = string.Format("Subscriptions/{0}/{1}/{2}", endpoint, messageType, client);
+
+            // First we need to convert the string into bytes, which
+            // means using a text encoder.
+            Encoder enc = System.Text.Encoding.Unicode.GetEncoder();
+
+            // Create a buffer large enough to hold the string
+            byte[] unicodeText = new byte[hunanReadableId.Length * 2];
+            enc.GetBytes(hunanReadableId.ToCharArray(), 0, hunanReadableId.Length, unicodeText, 0, true);
+
+            // Now that we have a byte array we can ask the CSP to hash it
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] result = md5.ComputeHash(unicodeText);
+
+            // Build the final string by converting each byte
+            // into hex and appending it to a StringBuilder
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                sb.Append(result[i].ToString("X2"));
+            }
+
+            // And return it
+            return sb.ToString();
         }
     }
 }
