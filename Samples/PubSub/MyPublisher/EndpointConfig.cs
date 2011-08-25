4f8d4e69d943f2a3f728315d507f45dd25d26b49
unknown <github@thejimmyp.com>
2011-08-25 11:28:01 +1000

    reverted PubSub sample

--- Samples/PubSub/MyPublisher/EndpointConfig.cs
+++ Samples/PubSub/MyPublisher/EndpointConfig.cs
@@ -1,24 +1,10 @@
 ﻿using MyMessages;
 using NServiceBus;
-using NServiceBus.Unicast.Subscriptions.Raven.Config;
+using NServiceBus.Grid.MessageHandlers;
+using NServiceBus.Sagas.Impl;
 
 namespace MyPublisher
 {
     class EndpointConfig : IConfigureThisEndpoint, AsA_Publisher,
-    IAmResponsibleForMessages<IEvent>, IAmResponsibleForMessages<EventMessage>{
-
-
-        
-    }
-
-    public class Busconfig: IWantCustomInitialization
-    {
-        public void Init()
-        {
-            NServiceBus.Configure.With()
-                .RavenSubscriptionStorage("Server")
-                .XmlSerializer()
-                .UnicastBus();
-        }
-    }
+    IAmResponsibleForMessages<IEvent>, IAmResponsibleForMessages<EventMessage>{}
 }
